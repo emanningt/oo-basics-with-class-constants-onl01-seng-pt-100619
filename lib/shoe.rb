1bbb6cbefb49aaa3043404803eecff9f1e364dc5
Eric Manning <emanningmusic@yahoo.com>
2019-11-02 17:25:25 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,6 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
-  BRANDS = []
 
   def initialize(brand)
     @brand = brand
@@ -13,9 +12,5 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
   
-  def brand=(brand)
-    @brand = brand
-    BRANDS << brand 
-  end
 
 end
